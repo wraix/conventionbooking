ca3431c771e052653bc287dddca7f07b8b74d7ca
Marc <7955883+wraix@users.noreply.github.com>
2022-10-23 09:53:03 +0200

    chore: fixed port issue

--- src/ConventionBooking.API/exec.sh
+++ src/ConventionBooking.API/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t convention-booking-api .
-docker run --init -p 3000:3000 -p 3001:3001 -it convention-booking-api
+docker run --init -p 8081:80 -it convention-booking-api
